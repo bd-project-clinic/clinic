b256128cc007ea36d574cde84a9ed03ee62e7f58
wojte <wojte@LAPTOP-PGOUTHBM>
2018-12-28 14:37:16 +0100

    Revert "Main"
    This reverts commit e01222467260a0332de6d03c8f639ac6d514b954.

--- demo-przychodnia_3warstwy-2017/load_data.sql
+++
@@ -1,5 +0,0 @@
-insert into dbo.Patient(FirstName , LastName) values ('Jan', 'Kowalski')
-insert into dbo.Patient(FirstName , LastName) values ('Krysztof', 'Nowak')
-insert into dbo.Patient(FirstName , LastName) values ('Roch', 'Karkowski')
-insert into dbo.Visit (IdPatient,[Description], Diagnosis) values (1, 'Wywiad1', 'Metastasis');
-
