ee56b40b956e5d609046f1cd5ea0e53f8854ec95
Paweł Buczyński <pawel.r.buczynski@outlook.com>
2019-01-23 13:51:36 +0100

    wykonczony admin v1

---
+++ demo-przychodnia_3warstwy-2017/Przychodnia-master/Admin.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Przychodnia
+{
+    partial class Admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewVisits = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVisits)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewVisits
+            // 
+            this.dataGridViewVisits.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewVisits.Location = new System.Drawing.Point(11, 147);
+            this.dataGridViewVisits.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewVisits.Name = "dataGridViewVisits";
+            this.dataGridViewVisits.RowTemplate.Height = 24;
+            this.dataGridViewVisits.Size = new System.Drawing.Size(516, 292);
+            this.dataGridViewVisits.TabIndex = 13;
+            this.dataGridViewVisits.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewVisits_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(608, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 66);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Dodaj użytkownika";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(608, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 48);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Zmień datę wygaśnięcia";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(503, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "role";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(542, 25);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(150, 20);
+            this.textBox3.TabIndex = 20;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(310, 25);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(150, 20);
+            this.textBox4.TabIndex = 19;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(253, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "DT_retire";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "uname";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.TabIndex = 16;
+            this.textBox1.Text = "K";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(698, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Szukaj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(608, 166);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(180, 48);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Usuń użytkownika";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridViewVisits);
+            this.Name = "Admin";
+            this.Text = "Admin";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVisits)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewVisits;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ demo-przychodnia_3warstwy-2017/Przychodnia-master/Admin.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BizzLayer;
+using DataLayer;
+namespace Przychodnia
+{
+    public partial class Admin : Form
+    {
+        User userSearchCriteria;
+        private void viewUsers()
+        {
+            // var result = RegistrationFacade.GetPatients(null);
+
+            // budowa kryteriów
+            userSearchCriteria = new User();
+            userSearchCriteria.uname = textBox1.Text;
+            userSearchCriteria.role = textBox3.Text;
+            if (textBox4.Text == "")
+                userSearchCriteria.DT_retire = default(DateTime);
+            else
+            userSearchCriteria.DT_retire = Convert.ToDateTime(textBox4.Text);
+
+            // ładowanie obiektu dataGridView
+            dataGridViewVisits.Columns.Clear();
+            // dataGridView1.AutoGenerateColumns = true;
+            dataGridViewVisits.DataSource = AdminFacade.GetUsers(userSearchCriteria);
+
+        }
+        public Admin()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridViewVisits_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DodajUsera frmDodajusera = new DodajUsera();
+            DialogResult res = frmDodajusera.ShowDialog(this);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewVisits.Rows.Count == 0)
+            {
+                MessageBox.Show("Brak Userów");
+                return;
+            }
+
+            
+            userSearchCriteria = new User();
+            userSearchCriteria.uname = (string)(dataGridViewVisits.Rows[dataGridViewVisits.CurrentCell.RowIndex].Cells[0].Value);
+
+
+
+            if (userSearchCriteria.uname == "")
+            {
+                MessageBox.Show("Wybierz pacjenta");
+                return;
+            }
+            else
+            {
+                MessageBox.Show(String.Format("Wybrano pacjenta o identyfikatorze {0}", userSearchCriteria.uname));
+
+            }
+            EdycjaUsera frmEdycjaPacjenta = new EdycjaUsera(userSearchCriteria.uname);
+            DialogResult res = frmEdycjaPacjenta.ShowDialog(this);
+            // button1_Click(null, null); :) :)
+            viewUsers();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            viewUsers();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (dataGridViewVisits.Rows.Count == 0)
+            {
+                MessageBox.Show("Brak użytkowników");
+                return;
+            }
+
+            string VisDeleteId = (string)(dataGridViewVisits.Rows[dataGridViewVisits.CurrentCell.RowIndex].Cells[0].Value);
+
+            DialogResult DeleteConfirm = MessageBox.Show("Czy na pewno chcesz usunąć wybranego użytkownika ?", "UWAGA", MessageBoxButtons.YesNo);
+            if (DeleteConfirm == DialogResult.Yes)
+            {
+                AdminFacade.DeleteUserData(VisDeleteId);
+                dataGridViewVisits.DataSource = AdminFacade.GetUsers(userSearchCriteria);
+            }
+            else if (DeleteConfirm == DialogResult.No)
+            {
+                return;
+            }
+
+        }
+    }
+}
